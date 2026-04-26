21910e26e86651b47972bd97923c4db7a6107ad7
Miguel Licla <liclainciso0@gmail.com>
2026-04-26 13:56:58 -0500

    habilitacion de cors

--- proyectoFlask_01/index.py
+++ proyectoFlask_01/index.py
@@ -6,6 +6,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+
 # Carga el modelo preentrenado TFLite (TensorFlow Lite)
 interp = tflite.Interpreter(model_path="model_c2f.tflite")
 interp.allocate_tensors()
